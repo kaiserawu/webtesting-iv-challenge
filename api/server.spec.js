b419d8869c2c121f31ec83d8f3a56bd202b645ae
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-27 19:00:25 -0700

    Add test file and prep assertions

---
+++ api/server.spec.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+  describe('GET /', () => {
+    it('should send a 200 status code', () => {
+
+    })
+
+    it('should send all of the requested data', () => {
+
+    })
+  })
+
+  describe('POST /', () => {
+    it('should send a 201 status code', () => {
+
+    })
+    
+    it('should save the data', () => {
+
+    })
+  })
+  
+  describe('DELETE /', () => {
+    it('should send a 200 status code', () => {
+
+    })
+
+    it('should remove the data', () => {
+      
+    })
+  })
+  
+});
