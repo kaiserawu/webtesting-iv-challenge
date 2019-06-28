0c3858f87946b2f71ad2d70b439f40174410ab4d
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-27 20:54:22 -0700

    Added tests for get endpoint

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const server = express();
 
+const dummyData = {
+  1: { name: "Carl", fruit: "banana" },
+  2: { name: "Jessica", fruit: "papaya" },
+  3: { name: "Murdock", fruit: "apple" }
+};
+
 server.use(express.json());
 
-
-
 module.exports = server;
+module.exports.dummyData = dummyData;
--- api/server.spec.js
+++ api/server.spec.js
@@ -4,12 +4,14 @@ const server = require('./server.js');
 
 describe('server.js', () => {
   describe('GET /', () => {
-    it('should send a 200 status code', () => {
-
+    it('should send a 200 status code', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
     })
 
-    it('should send all of the requested data', () => {
-
+    it('should send all of the requested data', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual(server.dummyData);
     })
   })
 
